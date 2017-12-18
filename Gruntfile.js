a0cf00390d8d78604d19e6110f6ba1c1616a8ed4
Taras Dolynyuk <trsdln@gmail.com>
2017-12-18 17:40:46 +0200

    Added character counter to dist

--- Gruntfile.js
+++ Gruntfile.js
@@ -184,7 +184,7 @@ module.exports = function(grunt) {
 //          "js/date_picker/picker.js",
 //          "js/date_picker/picker.date.js",
 //          "js/date_picker/picker.time.js",
-//          "js/character_counter.js",
+          "js/character_counter.js",
 //          "js/carousel.js",
 //          "js/tapTarget.js",
         ],
